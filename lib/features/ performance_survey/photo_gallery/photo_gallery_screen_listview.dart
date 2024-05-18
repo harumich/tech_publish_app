68eac91776fd085a096c25d75bb1dbf9dceba57e
harumich <talutalu.haru1486@gmail.com>
2024-05-19 01:12:21 +0900

    feat: ListViewでの実装

---
+++ lib/features/ performance_survey/photo_gallery/photo_gallery_screen_listview.dart
@@ -0,0 +1,45 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatelessWidget {
+  const ListViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final faker = Faker();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListViewで実装'),
+      ),
+      body: ListView(
+        children: [
+          for (var i = 0; i < _imageCount; i++)
+            _Photo(
+              url: faker.image.image(
+                keywords: ['nature', i.toString()],
+                random: true,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Photo extends StatelessWidget {
+  const _Photo({required this.url});
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      url,
+      width: double.infinity,
+      height: 200,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+const _imageCount = 100;
